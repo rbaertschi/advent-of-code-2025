5f64119615e4b6ba508b56dfe128dde4c0959e50
Roland Bärtschi <roland.baertschi@gmail.com>
2025-12-03 07:40:10 +0100

    prepare day 4

---
+++ src/test/java/ch/ebynaqon/aoc/aoc25/day04/Day04Test.java
@@ -0,0 +1,87 @@
+package ch.ebynaqon.aoc.aoc25.day04;
+
+import ch.ebynaqon.aoc.helper.RawProblemInput;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Solving puzzle https://adventofcode.com/2025/day/4
+class Day04Test {
+
+    @Test
+    void parseProblemInput() {
+        // given
+        RawProblemInput input = new RawProblemInput("""
+                42
+                """);
+
+        // when
+        var actual = Day04.parseProblem(input);
+
+        // then
+        assertThat(actual).isEqualTo(new ProblemInput(List.of(
+                new ProblemSample(42L)
+        )));
+    }
+
+    @Test
+    @Disabled
+    void solvePart1UsingExample() {
+        // given
+        RawProblemInput input = new RawProblemInput("""
+                42
+                """);
+
+        // when
+        var result = Day04.solvePart1(input);
+
+        // then
+        assertThat(result).isEqualTo(42);
+    }
+
+    @Test
+    @Disabled
+    void solvePart1() {
+        // given input from https://adventofcode.com/2025/day/4/input
+        RawProblemInput input = RawProblemInput.fromResource("/day04.txt");
+
+        // when
+        var result = Day04.solvePart1(input);
+
+        // then
+        assertThat(result).isEqualTo(42);
+    }
+
+    @Test
+    @Disabled
+    void solvePart2UsingExample() {
+        // given
+        RawProblemInput input = new RawProblemInput("""
+                42
+                """);
+
+        // when
+        var result = Day04.solvePart2(input);
+
+        // then
+        assertThat(result).isEqualTo(42);
+    }
+
+    @Test
+    @Disabled
+    void solvePart2() {
+        // given
+        RawProblemInput input = RawProblemInput.fromResource("/day04.txt");
+
+        // when
+        var result = Day04.solvePart2(input);
+
+        // then
+        assertThat(result).isEqualTo(42);
+    }
+
+}
+
