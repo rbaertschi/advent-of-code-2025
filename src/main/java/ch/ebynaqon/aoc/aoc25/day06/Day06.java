ba00d1fad439bf1288ce97a335b076898b0c5838
Roland Bärtschi <roland.baertschi@gmail.com>
2025-12-06 08:50:51 +0100

    day 6: refactor solution from part 1

--- src/main/java/ch/ebynaqon/aoc/aoc25/day06/Day06.java
+++ src/main/java/ch/ebynaqon/aoc/aoc25/day06/Day06.java
@@ -9,18 +9,40 @@ interface Day06 {
 
     static List<Operation> parseProblem(RawProblemInput input) {
         List<String> lines = input.getLines();
-        List<List<String>> parts = lines.stream().map(line -> List.of(line.trim().split("\\s+"))).toList();
-        int numberOfProblems = parts.getLast().size();
-        int numberOfNumbersPerProblem = lines.size() - 1;
+        String operatorLine = lines.getLast();
+        //noinspection OptionalGetWithoutIsPresent
+        int maxColumn = lines.stream().mapToInt(String::length).max().getAsInt() + 1;
+        List<String> numberLines = lines.subList(0, lines.size() - 1);
+        List<Integer> problemStartColumn = determineProblemStartColumns(operatorLine);
+        int numberOfProblems = problemStartColumn.size();
         ArrayList<Operation> problems = new ArrayList<>(numberOfProblems);
         for (int problemNumber = 0; problemNumber < numberOfProblems; problemNumber++) {
-            ArrayList<Integer> numbers = new ArrayList<>(numberOfNumbersPerProblem);
-            for (int currentNumberIndex = 0; currentNumberIndex < numberOfNumbersPerProblem; currentNumberIndex++) {
-                numbers.add(Integer.parseInt(parts.get(currentNumberIndex).get(problemNumber)));
-            }
-            problems.add(Operation.resolve(parts.getLast().get(problemNumber), numbers));
+            Integer startColumn = problemStartColumn.get(problemNumber);
+            Integer endColumn = (problemNumber == numberOfProblems - 1 ? maxColumn : problemStartColumn.get(problemNumber + 1)) - 1;
+            ArrayList<Integer> numbers = determineProblemInputNumbersHorizontal(numberLines, startColumn, endColumn);
+            problems.add(Operation.resolve(operatorLine.charAt(startColumn), numbers));
         }
         return problems;
+    }
+
+    private static List<Integer> determineProblemStartColumns(String operatorLine) {
+        List<Integer> problemStartColumn = new ArrayList<>();
+        for (int i = 0; i < operatorLine.length(); i++) {
+            char charInColumn = operatorLine.charAt(i);
+            if (charInColumn == '*' || charInColumn == '+') {
+                problemStartColumn.add(i);
+            }
+        }
+        return problemStartColumn;
+    }
+
+    private static ArrayList<Integer> determineProblemInputNumbersHorizontal(List<String> numberLines, int startColumn, Integer endColumn) {
+        int numberCount = numberLines.size();
+        ArrayList<Integer> numbers = new ArrayList<>(numberCount);
+        for (String line : numberLines) {
+            numbers.add(Integer.parseInt(line.substring(startColumn, Math.min(endColumn, line.length())).trim()));
+        }
+        return numbers;
     }
 
     static long solvePart1(RawProblemInput input) {
@@ -37,10 +59,10 @@ interface Day06 {
 sealed interface Operation {
     long compute();
 
-    static Operation resolve(String op, List<Integer> numbers) {
+    static Operation resolve(char op, List<Integer> numbers) {
         return switch (op) {
-            case "+" -> new Addition(numbers);
-            case "*" -> new Multiplication(numbers);
+            case '+' -> new Addition(numbers);
+            case '*' -> new Multiplication(numbers);
             default -> throw new IllegalArgumentException("Unknown operation: " + op);
         };
     }
