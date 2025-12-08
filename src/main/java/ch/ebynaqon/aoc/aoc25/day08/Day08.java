f93075d8fa321ae0d9f7b92c61d1f28a6400ffc1
Roland Bärtschi <roland.baertschi@gmail.com>
2025-12-08 07:31:36 +0100

    day 8: first implementation for part 1

--- src/main/java/ch/ebynaqon/aoc/aoc25/day08/Day08.java
+++ src/main/java/ch/ebynaqon/aoc/aoc25/day08/Day08.java
@@ -2,8 +2,12 @@ package ch.ebynaqon.aoc.aoc25.day08;
 
 import ch.ebynaqon.aoc.helper.RawProblemInput;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 interface Day08 {
 
@@ -23,9 +27,52 @@ interface Day08 {
         );
     }
 
-    static long solvePart1(RawProblemInput input) {
-        List<JunctionBox> problem = parseProblem(input);
-        return 0;
+    static long solvePart1(RawProblemInput input, int maxConnections) {
+        List<JunctionBox> junctionBoxes = parseProblem(input);
+        List<Connection> connections = new ArrayList<>();
+        for (int i = 0; i < junctionBoxes.size() - 1; i++) {
+            JunctionBox fromBox = junctionBoxes.get(i);
+            for (int j = i + 1; j < junctionBoxes.size(); j++) {
+                JunctionBox toBox = junctionBoxes.get(j);
+                double distance = fromBox.distanceTo(toBox);
+                connections.add(new Connection(i, j, distance));
+            }
+        }
+        List<Connection> smallestDistanceConnections = connections.stream()
+                .sorted(Comparator.comparing(Connection::distance))
+                .toList();
+        int[] setIndex = new int[junctionBoxes.size()];
+        for (int i = 0; i < junctionBoxes.size(); i++) {
+            setIndex[i] = i;
+        }
+        int nextSetId = 1;
+        int numberOfNewConnections = 0;
+        for (var connection : smallestDistanceConnections) {
+            if (numberOfNewConnections >= maxConnections) {
+                break;
+            }
+            int fromBox = connection.fromBoxId();
+            int toBox = connection.toBoxId();
+            var fromSet = setIndex[fromBox];
+            var toSet = setIndex[toBox];
+            if (fromSet != toSet) {
+                for (int i = 0; i < junctionBoxes.size(); i++) {
+                    if (setIndex[i] == toSet) {
+                        setIndex[i] = fromSet;
+                    }
+                }
+                numberOfNewConnections++;
+            }
+        }
+        Collection<Long> setSizes = Arrays.stream(setIndex)
+                .filter(i -> i > 0)
+                .mapToObj(i -> (Integer) i)
+                .collect(Collectors.groupingBy(i -> i, Collectors.counting()))
+                .values();
+        return setSizes.stream()
+                .sorted(Comparator.comparingLong(Long::longValue).reversed())
+                .limit(3)
+                .reduce(1L, (a, b) -> a * b);
     }
 
     static long solvePart2(RawProblemInput input) {
@@ -35,5 +82,13 @@ interface Day08 {
 }
 
 record JunctionBox(int x, int y, int z) {
+
+    public double distanceTo(JunctionBox other) {
+        return Math.sqrt((other.x - x) * (other.x - x)
+                         + (other.y - y) * (other.y - y)
+                         + (other.z - z) * (other.z - z));
+    }
 }
 
+record Connection(int fromBoxId, int toBoxId, double distance) {
+}
--- src/test/java/ch/ebynaqon/aoc/aoc25/day08/Day08Test.java
+++ src/test/java/ch/ebynaqon/aoc/aoc25/day08/Day08Test.java
@@ -66,18 +66,36 @@ class Day08Test {
     }
 
     @Test
-    @Disabled
     void solvePart1UsingExample() {
         // given
         RawProblemInput input = new RawProblemInput("""
-                42
+                162,817,812
+                57,618,57
+                906,360,560
+                592,479,940
+                352,342,300
+                466,668,158
+                542,29,236
+                431,825,988
+                739,650,466
+                52,470,668
+                216,146,977
+                819,987,18
+                117,168,530
+                805,96,715
+                346,949,466
+                970,615,88
+                941,993,340
+                862,61,35
+                984,92,344
+                425,690,689
                 """);
 
         // when
-        var result = Day08.solvePart1(input);
+        var result = Day08.solvePart1(input, 9);
 
         // then
-        assertThat(result).isEqualTo(42);
+        assertThat(result).isEqualTo(40);
     }
 
     @Test
@@ -87,7 +105,7 @@ class Day08Test {
         RawProblemInput input = RawProblemInput.fromResource("/day08.txt");
 
         // when
-        var result = Day08.solvePart1(input);
+        var result = Day08.solvePart1(input, 1000);
 
         // then
         assertThat(result).isEqualTo(42);
